7f03df51b9faabb642d54daec74d7b24ecbbb3a0
tristanbyram <tbyram@deloitte.com>
2022-11-03 21:12:56 +0000

    Original File Update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
+
 console.log(`process.env.SERVER = ${process.env.SERVER}`);
+
 // get the environment variable, but default to localhost:8082 if its not set
+
 const SERVER = process.env.SERVER ? process.env.SERVER : "http://localhost:8082";
 
 // express is a nodejs web server
